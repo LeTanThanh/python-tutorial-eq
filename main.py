199f7efb660d21828c7e66b53a9827bfeac5d716
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-29 00:36:05 +0700

    Python __eq__

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+from person import Person
+
+# Python __eq__
+
+john = Person("John", "Doe", 25)
+jane = Person("Jane", "Doe", 25)
+
+print(john is jane)
+print(john == jane)
---
+++ person.py
@@ -0,0 +1,11 @@
+class Person:
+  def __init__(self, first_name, last_name, age) -> None:
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+
+  def __eq__(self, other) -> bool:
+    if isinstance(other, Person):
+      return self.age == other.age
+
+    return False
